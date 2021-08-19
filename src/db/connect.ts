18819d17af29aa23e73c19ff515e2af488145734
visionary <wangmin@ebupt.com>
2021-08-19 17:44:18 +0800

    .

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,10 @@
 const AWS = require("aws-sdk");
-
+// const myCredentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: "IDENTITY_POOL_ID"});
 AWS.config.update({
     region: "us-east-2",
+    accessKeyId: "AKIA6IH5D3SRP5QRUJFP",
+    secretAccessKey: "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"
+    // credentials: myCredentials
 });
 
 export const dynamodb = new AWS.DynamoDB();
