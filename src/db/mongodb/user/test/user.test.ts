4a187e36dce04c640de12157e5a580d99ce5896f
Mia <min_0610@icloud.com>
2022-04-07 14:54:31 +0800

    user.test.ts

--- src/db/mongodb/user/test/user.test.ts
+++ src/db/mongodb/user/test/user.test.ts
@@ -2,7 +2,7 @@ import {getUser } from "../../index";
 
 describe("user db", () => {
     test("增改查", async () => {
-        const user = await getUser();
+        const user = await getUser("123");
         console.log(user);
     });
 });
